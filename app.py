12d1e22ee159b3db589082e0b8a82acb9281a034
Sara Nettle <nettles@classwork.engr.oregonstate.edu>
2024-08-04 22:21:54 -0700

    added GET and POST function to Neighbors table

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import os
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'classmysql.engr.oregonstate.edu'
-app.config['MYSQL_USER'] = 'cs340_xxxx'
-app.config['MYSQL_PASSWORD'] = 'xxxx' #last 4 of onid
-app.config['MYSQL_DB'] = 'cs340_xxxx'
+app.config['MYSQL_USER'] = 'cs340_nettles'
+app.config['MYSQL_PASSWORD'] = '4130' #last 4 of onid
+app.config['MYSQL_DB'] = 'cs340_nettles'
 app.config['MYSQL_CURSORCLASS'] = "DictCursor"
 
 
@@ -29,13 +29,38 @@ def visits():
 def visit_types():
     return render_template("visit_types.j2")
 
-@app.route('/neighbors')
-def neighbors():
-    return render_template("neighbors.j2")
 
+@app.route('/neighbors', methods=["GET"])
+def neighbors():
+
+    if request.method == "GET":
+        query = "SELECT neighborID, firstName, lastName, neighborPhone FROM Neighbors ORDER BY neighborID"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+    return render_template("neighbors.j2", data=data)
+
+@app.route('/add_neighbor', methods=["GET", "POST"])
+def add_neighbor():
+
+    if request.method == "POST":
+
+        if request.form.get("add_neighbor"):
+            # grab user form inputs
+            firstName = request.form["firstName"]
+            lastName = request.form["lastName"]
+            neighborPhone = request.form["neighborPhone"]
+            
+            query = "INSERT INTO Neighbors (firstName, lastName, neighborPhone) VALUES (%s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (firstName, lastName, neighborPhone))
+            mysql.connection.commit()
+
+    return render_template("add_neighbor.j2")
 
 # Listener
 if __name__ == "__main__":
 
     #Start the app on port 3000, it will be different once hosted
-    app.run(port=3000, debug=True)
+    app.run(port=31193, debug=True)
